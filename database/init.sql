dbe4acac073bc2248b7498c29716025f8f9d0584
Muhammad Awwad <hammode.awad@gmail.com>
2020-12-27 11:50:52 +0200

    setup files

---
+++ database/init.sql
@@ -0,0 +1,26 @@
+BEGIN;
+
+DROP TABLE IF EXISTS users,business CASCADE;
+
+CREATE TABLE users(
+id SERIAL PRIMARY KEY,
+email VARCHAR(255) ,
+phone VARCHAR(255) NOT NULL,
+firstname VARCHAR(255) NOT NULL,
+lastname VARCHAR(255) NOT NULL,
+pass VARCHAR(255) NOT NULL,
+isBusinessOwner BOOLEAN NOT NULL 
+);
+
+
+CREATE TABLE business(
+    businessName VARCHAR(255) NOT NULL,
+    ownerId INTEGER REFERENCES users(id),
+    phone VARCHAR(255) NOT NULL,
+    businessAddress VARCHAR(255) NOT NULL,
+    geoLocation VARCHAR(255) NOT NULL
+);
+
+
+
+COMMIT;
