413b9f5b74d54f1dd8c21807869da70e9e00c8d6
mj majadly <majadly@nevada.unr.edu>
2020-12-28 15:09:25 +0200

    addingerror

--- model/businessModel.js
+++ model/businessModel.js
@@ -1,8 +1,8 @@
-const db = require("../database/connection");
+const db = require('../database/connection');
 
 //getBusiness
 const getBusiness = (name) => {
-  return db.query("SELECT * FROM business").then((businesses) => {
+  return db.query('SELECT * FROM business').then((businesses) => {
     if (name) {
       const businessesArr = businesses.rows;
       const filteredArr = businessesArr.filter(
@@ -28,7 +28,7 @@ const newBusiness = ({
 }) => {
   return db
     .query(
-      "INSERT INTO business (businessname,ownerid,phone,businessaddress,geolocation) VALUES ($1,$2,$3,$4,$5) RETURNING *;",
+      'INSERT INTO business (businessname,ownerid,phone,businessaddress,geolocation) VALUES ($1,$2,$3,$4,$5) RETURNING *;',
       [businessname, ownerid, phone, businessaddress, geolocation]
     )
     .then((business) => {
@@ -42,19 +42,19 @@ const newBusiness = ({
 //createCalendarTable, this function creates a table for a specific month and buusiness id
 const createCalendarTable = (calendar, businessId) => {
   const { month, days } = calendar;
-  const tableName = month + "_" + businessId;
+  const tableName = month + '_' + businessId;
   db.connect((err) => {
     if (err) throw err;
-    console.log("Connected!");
+    console.log('Connected!');
     //Now in the following command we will create the table
     db.query(
       `CREATE TABLE ${tableName} (id SERIAL PRIMARY KEY,workinghours TEXT, isWorking Boolean, appointments VARCHAR(255), diff INTEGER)`
     )
       .then(() => {
-        console.log("Table Created");
+        console.log('Table Created');
         initDays(days, tableName);
       })
-      .catch((err) => console.log("Error: ", err));
+      .catch((err) => console.log('Error: ', err));
   });
 };
 
@@ -62,17 +62,17 @@ const createCalendarTable = (calendar, businessId) => {
 const initDays = (days, tableName) => {
   for (let i = 0; i < days.length; i++) {
     const stringifiedWorkingHours = JSON.stringify(days[i].workinghours);
-    console.log("STRINGIFIEEED: ", stringifiedWorkingHours);
+    console.log('STRINGIFIEEED: ', stringifiedWorkingHours);
     db.query(
       `INSERT INTO ${tableName} (workinghours,isworking,diff) VALUES($1,$2,$3)`,
       [stringifiedWorkingHours, days[i].isworking, days[i].diff]
-    ).then(() => console.log("Added"));
+    ).then(() => console.log('Added'));
   }
 };
 
-db.query("SELECT workinghours FROM august_40 WHERE id=1").then((object) => {
-  console.log("OBJEEEEEEEEEEEEEEECT: ", object.rows[0]);
-});
+// db.query("SELECT workinghours FROM august_40 WHERE id=1").then((object) => {
+//   console.log("OBJEEEEEEEEEEEEEEECT: ", object.rows[0]);
+// });
 
 //editBusiness
 /*const editBusiness = (businessId, businessId) => {
@@ -98,7 +98,7 @@ db.query("SELECT workinghours FROM august_40 WHERE id=1").then((object) => {
 
 const updateCalendarTable = (calendar, businessId) => {
   const { month, days } = calendar;
-  const tableNameToUpdate = month + "_" + businessId;
+  const tableNameToUpdate = month + '_' + businessId;
   for (let i = 0; i < days.length; i++) {
     db.query(`INSERT INTO ${tableNameToUpdate} ()`);
   }
--- model/userModel.js
+++ model/userModel.js
@@ -1,4 +1,4 @@
-const db = require("../database/connection");
+const db = require('../database/connection');
 
 function addUser({ email, phone, firstname, lastname, pass, isBusinessOwner }) {
   return db.query(
@@ -9,13 +9,13 @@ function addUser({ email, phone, firstname, lastname, pass, isBusinessOwner }) {
 
 function getUserByPhone(phone) {
   return db.query(`SELECT * FROM users WHERE phone=$1`, [phone]).then((res) => {
-    if (!res.rows.length) throw new Error("No user with this phone number");
+    if (!res.rows.length) throw new Error('No user with this phone number');
     return res.rows[0];
   });
 }
 function getUserByEmail(email) {
   return db.query(`SELECT * FROM users WHERE email=$1`, [email]).then((res) => {
-    if (!res.rows.length) throw new Error("No user with this email");
+    if (!res.rows.length) throw new Error('No user with this email');
     return res.rows[0];
   });
 }
@@ -33,10 +33,14 @@ function checkValid(email, phone) {
 // adding appointments to the users' table:
 
 function getAppointments(client_id) {
+  // console.log('usermodel id: ', client_id);
   return db
     .query(`SELECT myAppointments FROM users WHERE id=$1`, [client_id])
     .then((client) => {
-      client.rows[0];
+      if (!client.rows.length) {
+        throw new Error('User not found');
+      }
+      return client.rows[0];
     });
 }
 
